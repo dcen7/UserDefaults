3032f551753b7ec5e285aceba58a360fe8eed087
Deniz <mdcengiz@gmail.com>
2020-05-15 12:16:00 -0500

    Add design for UICollectionView cell and data sources

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Mehmet Deniz Cengiz on 5/15/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var ImageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
---
+++ Project10/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Mehmet Deniz Cengiz on 5/15/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        
+        return cell
+    }
+
+
+}
+
